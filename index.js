0f41bdef1395d3c09355fa846570954dbfbe9eff
Brycew30 <brycew30@gmail.com>
2020-09-25 09:22:19 -0500

    Done.

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
-const testVar = {}
-
-function testFunc() {
-  return "hi"
+const superbowlWin = (record) => {
+  let result = record.find(record => record.result === "W")
+  if (!!result) {
+    return result.year
+  } else {
+    return undefined
+  }
 }
