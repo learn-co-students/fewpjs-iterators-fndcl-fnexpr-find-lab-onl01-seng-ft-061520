17ca6af11028859bbfaaacb836cf103e78e8089d
Michael Rosenberg <rosenbergmj1@gmail.com>
2020-09-26 18:22:54 -0400

    Done.

--- index.js
+++ index.js
@@ -3,3 +3,10 @@ const testVar = {}
 function testFunc() {
   return "hi"
 }
+
+
+
+superbowlWin = (record) => {
+  let result = record.find( record => record.result === "W" )
+  return !!result ? result.year : undefined
+}
