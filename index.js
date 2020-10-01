2f740d43d56c168c4b7e79f2491e6cec43509423
Samuel Grasse-Haroldsen <sgrasseharoldsen@gmail.com>
2020-10-01 14:16:33 -0700

    Done.

--- index.js
+++ index.js
@@ -3,3 +3,12 @@ const testVar = {}
 function testFunc() {
   return "hi"
 }
+
+function superbowlWin(superbowls){
+    let result = superbowls.find( (superbowl) => { return superbowl.result === "W"} )
+    if(!!result){
+	return result.year;
+    }else{
+	return undefined;
+    }
+}
